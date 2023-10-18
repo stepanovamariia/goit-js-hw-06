619d74c1431f166df60d49befb6cbc74f840cf80
stepanovamariia <mariia.alexsa@gmail.com>
2023-10-18 17:36:50 +0300

    task-8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const loginForm = document.querySelector('.login-form');
 
+loginForm.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+	event.preventDefault();
+	const form = event.target;
+	const email = form.elements.email.value;
+	const password = form.elements.password.value;
+
+	if (email === '' || password === '') {
+		return alert('Будь ласка, заповність всі поля!');
+	} else {
+		const values = {
+			Email: form.elements.email.value,
+			Password: form.elements.password.value,
+		};
+		console.log(values);
+		loginForm.reset();
+	}
+}
