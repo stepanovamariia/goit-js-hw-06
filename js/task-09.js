2e25125c3db4c725fa2660b5b0abfa127ef4c6c2
stepanovamariia <mariia.alexsa@gmail.com>
2023-10-18 17:52:58 +0300

    tack-9

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,14 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+	return `#${Math.floor(Math.random() * 16777215)
+		.toString(16)
+		.padStart(6, 0)}`;
 }
+
+const body = document.querySelector('body');
+const colorValue = document.querySelector('.color');
+const changeColorButton = document.querySelector('.change-color');
+
+changeColorButton.addEventListener('click', () => {
+	body.style.backgroundColor = getRandomHexColor();
+	colorValue.textContent = body.style.backgroundColor;
+});
