1c20c4fd6b769a6116e1815d7e490bc73b7a6889
stepanovamariia <mariia.alexsa@gmail.com>
2023-10-19 10:23:29 +0300

    task-2

--- js/task-02.js
+++ js/task-02.js
@@ -8,11 +8,13 @@ const ingredients = [
 ];
 
 const list = document.querySelector('#ingredients');
+const newEl = [];
 
 for (let ingredient of ingredients) {
 	const listItem = document.createElement('li');
 	listItem.textContent = ingredient;
-	listItem.classList.add('ingridient');
-
-	list.append(listItem);
+	listItem.classList.add('item');
+	newEl.push(listItem);
 }
+
+list.append(...newEl);
